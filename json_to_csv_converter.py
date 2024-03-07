1bd6a5897386eec1b9b3afba82fda60af1961c11
zoybzo <zoybzo@outlook.com>
2024-03-07 12:46:30 +0800

    Updated the package and some lines. Now you can run the converter with python3

--- json_to_csv_converter.py
+++ json_to_csv_converter.py
@@ -4,21 +4,29 @@
 For more information on the Yelp Dataset Challenge please visit http://yelp.com/dataset_challenge
 
 """
+import sys
 import argparse
 import collections
 import csv
-import simplejson as json
+import json
+
+# import simplejson as json
+from loguru import logger
+from tqdm import tqdm
 
 
 def read_and_write_file(json_file_path, csv_file_path, column_names):
     """Read in the json dataset file and write it out to a csv file, given the column names."""
-    with open(csv_file_path, 'wb+') as fout:
+    # logger.debug("column_names: {0}".format(column_names))
+    # logger.debug(f"type of column_names: {type(column_names)}")
+    with open(csv_file_path, "w+") as fout:
         csv_file = csv.writer(fout)
         csv_file.writerow(list(column_names))
         with open(json_file_path) as fin:
-            for line in fin:
+            for line in tqdm(fin):
                 line_contents = json.loads(line)
                 csv_file.writerow(get_row(line_contents, column_names))
+
 
 def get_superset_of_column_names_from_file(json_file_path):
     """Read in the json dataset file and return the superset of column names."""
@@ -26,12 +34,12 @@ def get_superset_of_column_names_from_file(json_file_path):
     with open(json_file_path) as fin:
         for line in fin:
             line_contents = json.loads(line)
-            column_names.update(
-                    set(get_column_names(line_contents).keys())
-                    )
+            # logger.debug(f"line_contents: {line_contents}")
+            column_names.update(set(get_column_names(line_contents).keys()))
     return column_names
 
-def get_column_names(line_contents, parent_key=''):
+
+def get_column_names(line_contents, parent_key=""):
     """Return a list of flattened key names given a dict.
 
     Example:
@@ -49,19 +57,18 @@ def get_column_names(line_contents, parent_key=''):
 
     """
     column_names = []
-    for k, v in line_contents.iteritems():
+    for k, v in line_contents.items():
         column_name = "{0}.{1}".format(parent_key, k) if parent_key else k
-        if isinstance(v, collections.MutableMapping):
-            column_names.extend(
-                    get_column_names(v, column_name).items()
-                    )
+        if isinstance(v, collections.abc.MutableMapping):
+            column_names.extend(get_column_names(v, column_name).items())
         else:
             column_names.append((column_name, v))
     return dict(column_names)
 
+
 def get_nested_value(d, key):
     """Return a dictionary item given a dictionary `d` and a flattened key from `get_column_names`.
-    
+
     Example:
 
         d = {
@@ -73,51 +80,65 @@ def get_nested_value(d, key):
         key = 'a.b'
 
         will return: 2
-    
+
     """
-    if '.' not in key:
-        if key not in d:
+    if "." not in key:
+        if d is None or key not in d:
             return None
         return d[key]
-    base_key, sub_key = key.split('.', 1)
+    ss = key.split(".", 1)
+    base_key, sub_key = ss[0], ss[1]
+    # logger.debug(f"sub_key: {sub_key}")
     if base_key not in d:
         return None
+    base_key = str(base_key)
     sub_dict = d[base_key]
     return get_nested_value(sub_dict, sub_key)
+
 
 def get_row(line_contents, column_names):
     """Return a csv compatible row given column names and a dict."""
     row = []
     for column_name in column_names:
+        # logger.debug(f"column_name: {column_name}")
+        # logger.debug(f"line contents: {line_contents}")
         line_value = get_nested_value(
-                        line_contents,
-                        column_name,
-                        )
-        if isinstance(line_value, unicode):
-            row.append('{0}'.format(line_value.encode('utf-8')))
-        elif line_value is not None:
-            row.append('{0}'.format(line_value))
+            line_contents,
+            column_name,
+        )
+        # logger.debug(f"type of line_value: {type(line_value)}")
+        # logger.debug(f"line_value: {line_value}")
+        # if isinstance(line_value, unicode):
+        #     row.append("{0}".format(line_value.encode("utf-8")))
+        # elif line_value is not None:
+        if line_value is not None:
+            row.append("{0}".format(line_value))
         else:
-            row.append('')
+            row.append("none")
     return row
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     """Convert a yelp dataset file from json to csv."""
 
     parser = argparse.ArgumentParser(
-            description='Convert Yelp Dataset Challenge data from JSON format to CSV.',
-            )
+        description="Convert Yelp Dataset Challenge data from JSON format to CSV.",
+    )
 
     parser.add_argument(
-            'json_file',
-            type=str,
-            help='The json file to convert.',
-            )
+        "json_file",
+        type=str,
+        help="The json file to convert.",
+    )
 
     args = parser.parse_args()
 
     json_file = args.json_file
-    csv_file = '{0}.csv'.format(json_file.split('.json')[0])
+    csv_file = "{0}.csv".format(json_file.split(".json")[0])
+    # Set the level of logger in loguru
+    logger.remove()
+    # logger.add(sys.stderr, level="DEBUG")
+    logger.add(sys.stderr, level="INFO")
 
     column_names = get_superset_of_column_names_from_file(json_file)
     read_and_write_file(json_file, csv_file, column_names)
